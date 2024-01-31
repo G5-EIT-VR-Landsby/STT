3de1b7262a6ae87687fad04e718c7af0d8cae967
vyjur <idyllistoic@gmail.com>
2024-01-31 14:04:22 +0100

    FIX

--- norsk_dialekt.py
+++
@@ -1,8 +0,0 @@
-# Load model directly
-from transformers import AutoModel, AutoProcessor, AutoModelForSpeechSeq2Seq, AutoTokenizer
-
-processor = AutoProcessor.from_pretrained("NbAiLab/nb-whisper-tiny-beta")
-model = AutoModel.from_pretrained("NbAiLab/nb-whisper-tiny-beta")
-tokenizer = AutoTokenizer.from_pretrained("NbAiLab/nb-whisper-tiny-beta")
-model.save_pretrained("./norsk_model.pt", save_weights_only=True)
-tokenizer.save_pretrained("./norsk_model")
